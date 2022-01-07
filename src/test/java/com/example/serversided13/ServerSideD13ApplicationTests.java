83d35f0993121fd82759efafe2ac53e49fe83977
boxyboxy <eugenengboxiang@gmail.com>
2022-01-07 11:39:30 +0800

    initialised spring booty app

---
+++ src/test/java/com/example/serversided13/ServerSideD13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serversided13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideD13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
